d924826a5552e16ab40ace54e584600b3014ed36
Nanako-Murata <mokalate.nanako@gmail.com>
2026-07-24 16:16:23 +0900

    commit

--- demo/src/main/java/com/example/demo/controller/HouseController.java
+++ demo/src/main/java/com/example/demo/controller/HouseController.java
@@ -16,6 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.demo.entity.House;
+import com.example.demo.form.HouseEditForm;
 import com.example.demo.form.HouseRegisterForm;
 import com.example.demo.repository.HouseRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -81,6 +82,35 @@ public class HouseController {
         redirectAttributes.addFlashAttribute("successMessage", "民泊を予約しました");
         return "redirect:/admin/houses";
 
+    }
+
+    // 民泊の編集画面の表示
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Integer id, Model model) {
+        House house = houseRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "民宿が見つかりません"));
+        String imageName = house.getImageName();
+        HouseEditForm houseEditForm = new HouseEditForm(house.getId(), house.getName(), null, house.getDescription(),
+                house.getPrice(), house.getCapacity(), house.getPostalCode(), house.getAddress(),
+                house.getPhoneNumber());
+        model.addAttribute("imageName", imageName);
+        model.addAttribute("houseEditForm", houseEditForm);
+        return "admin/houses/edit";
+    }
+
+    // 民泊の情報編集
+    @PostMapping("/{id}/update")
+    public String update(@ModelAttribute @Validated HouseEditForm houseEditForm, BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        // TODO: process POST request
+        if(bindingResult.hasErrors()){
+            return "admin/houses/edit";
+
+        }
+        houseService.update(houseEditForm);
+        redirectAttributes.addFlashAttribute("successMessage", "民泊情報を編集しました");
+
+        return "redirect:/admin/houses";
     }
 
 }
--- demo/src/main/java/com/example/demo/service/HouseService.java
+++ demo/src/main/java/com/example/demo/service/HouseService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.entity.House;
+import com.example.demo.form.HouseEditForm;
 import com.example.demo.form.HouseRegisterForm;
 import com.example.demo.repository.HouseRepository;
 
@@ -21,22 +22,22 @@ import jakarta.transaction.Transactional;
 public class HouseService {
     private final HouseRepository houseRepository;
 
-    public HouseService(HouseRepository houseRepository){
-        this.houseRepository=houseRepository;
+    public HouseService(HouseRepository houseRepository) {
+        this.houseRepository = houseRepository;
     }
 
     @Transactional
-    public void create(HouseRegisterForm houseRegisterForm){
+    public void create(HouseRegisterForm houseRegisterForm) {
         House house = new House();
         MultipartFile imageFile = houseRegisterForm.getImageFile();
 
-        if(!imageFile.isEmpty()){
+        if (!imageFile.isEmpty()) {
             String imageName = imageFile.getOriginalFilename();
-            if(imageName == null){
+            if (imageName == null) {
                 imageName = "unknown";
             }
             String hashedImageName = generateNewFileName(imageName);
-            Path filePath =Paths.get("src/main/resources/static/storage/" + hashedImageName);
+            Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
             copyImageFile(imageFile, filePath);
             house.setImageName(hashedImageName);
 
@@ -52,27 +53,50 @@ public class HouseService {
         houseRepository.save(house);
     }
 
-    //UUIDを使って生成したファイル名を返す
-    public String generateNewFileName(String fileName){
+    // UUIDを使って生成したファイル名を返す
+    public String generateNewFileName(String fileName) {
         String extension = "";
         int dotIndex = fileName.lastIndexOf(".");
-        if(dotIndex != -1){
-            extension= fileName.substring(dotIndex);
+        if (dotIndex != -1) {
+            extension = fileName.substring(dotIndex);
 
         }
         return UUID.randomUUID() + extension;
 
     }
-    //画像ファイルを指定しファイルにコピーする
-    public void copyImageFile(MultipartFile imageFile, Path filePath){
-        try{
+
+    // 画像ファイルを指定しファイルにコピーする
+    public void copyImageFile(MultipartFile imageFile, Path filePath) {
+        try {
             Files.copy(imageFile.getInputStream(), filePath);
 
-        }catch(IOException e){
+        } catch (IOException e) {
             throw new UncheckedIOException("画像の保存に失敗しました", e);
-            
 
         }
+    }
+
+    // 民泊を更新する
+    @Transactional
+    public void update(HouseEditForm houseEditForm) {
+        House house = houseRepository.findById(houseEditForm.getId()).orElseThrow();
+        MultipartFile imageFile = houseEditForm.getImageFile();
+        if (!imageFile.isEmpty()) {
+            String imageName = imageFile.getOriginalFilename();
+            String hashedImageName = generateNewFileName(imageName);
+            Path filePath = Paths.get("src/main/resources/static/storage/" + hashedImageName);
+            copyImageFile(imageFile, filePath);
+            house.setImageName(hashedImageName);
+        }
+        house.setName(houseEditForm.getName());
+        house.setDescription(houseEditForm.getDescription());
+        house.setPrice(houseEditForm.getPrice());
+        house.setCapacity(houseEditForm.getCapacity());
+        house.setPostalCode(houseEditForm.getPostalCode());
+        house.setAddress(houseEditForm.getAddress());
+        house.setPhoneNumber(houseEditForm.getPhoneNumber());
+
+        houseRepository.save(house);
     }
 
 }
